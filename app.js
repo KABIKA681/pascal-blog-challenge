8492eeb4179cf23b3427ddeafe1aa338d1d00a3f
pascalmpang <pascalkabikampan@gmail.com>
2021-10-19 14:06:15 +0200

    second commit

--- app.js
+++ app.js
@@ -1,63 +1,33 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/posts?userId=1'
+const API_URL = 'https://jsonplaceholder.typicode.com/posts/'
 const randomCardsPhoto = document.querySelector('.cards-photo-container');
 
 
 async function getRandomData() {
     randomCardsPhoto.innerHTML = '';
     const response = await fetch(API_URL);
-    const json = await response.json();
-    console.log(json)
+    const posts = await response.json();
+    const bigPosts = posts.filter((post) => post.id % 4 == 0);
+    console.log(bigPosts);
 
-    json.forEach(posts => {
+    bigPosts.forEach(posts => {
         randomCardsPhoto.innerHTML += `
         <div class="cards-photos">
                     <div class="photo">
                         <img src="/asset/pexels-greta-hoffman-9706062.jpg" alt="">
                             </div>
                             <div class="param-photo">
-                            <div class="date">${posts.id}</div>
+                            <div class="date">19th October 2021</div>
                             <div class="content3">
                             <h3>${posts.title}</h3>
                             </div>
                             <div class="content4">
                             <h4> ${posts.body}</h4>
                             </div>
-                            <button>See in details</button>
+                            <button onclick="getOnePost(${posts.id})">See in details</button>
                                 </div>  
-                </div>
-                <div class="cards-photos">
-                    <div class="photo">
-                        <img src="/asset/pexels-greta-hoffman-9705923.jpg" alt="">
-                            </div>
-                            <div class="param-photo">
-                            <div class="date">${posts.id}</div>
-                            <div class="content3">
-                            <h3>${posts.title}</h3>
-                            </div>
-                            <div class="content4">
-                            <h4> ${posts.body}</h4>
-                            </div>
-                            <button>See in details</button>
-                                </div>  
-                </div>
-                <div class="cards-photos">
-                    <div class="photo">
-                        <img src="/asset/pexels-greta-hoffman-9706059.jpg" alt="">
-                            </div>
-                            <div class="param-photo">
-                            <div class="date">${posts.id}</div>
-                            <div class="content3">
-                            <h3>${posts.title}</h3>
-                            </div>
-                            <div class="content4">
-                            <h4> ${posts.body}</h4>
-                            </div>
-                            <button>See in details</button>
-                                </div>  
-                </div>        
+                </div>                     
         `
     });
    
 }
-
 getRandomData();
